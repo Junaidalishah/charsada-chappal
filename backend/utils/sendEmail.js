26bf513965548037bcf9db5a76ffefefece65a5b
Junaid Ali Shah <sjunaidali47@gmail.com>
2026-06-09 01:01:50 +0500

    switch to resend domain email

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ to, subject, html }) => {
   try {
     const data = await resend.emails.send({
-      from: "Charsadda Chappal <onboarding@resend.dev>",
+      from: "Charsadda Chappal <info@charsaddachappal.store>",
       to,
       subject,
       html,
