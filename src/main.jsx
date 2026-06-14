e82463fc511e4c8a6e8997a7e020424e92a41b17
Junaid Ali Shah <sjunaidali47@gmail.com>
2026-06-14 10:37:55 +0500

    Meta pixel

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
-
 import ReactGA from "react-ga4";
+import ReactPixel from "react-facebook-pixel";
 
 import "./index.css";
-
 import App from "./App.jsx";
-
 import { ToastProvider } from "./context/ToastContext";
+
+// GA4 init
 ReactGA.initialize("G-DPNZJM55JZ");
+
+// Meta Pixel init
+const pixelOptions = {
+  autoConfig: true,
+  debug: false,
+};
+
+ReactPixel.init("3263994340476127", pixelOptions);
+ReactPixel.pageView();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ToastProvider>
