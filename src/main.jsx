68de1142ca769c47aae2771c4e4d2b9bc993764e
Junaid Ali Shah <sjunaidali47@gmail.com>
2026-06-14 10:45:01 +0500

    Meta pixel

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import ReactGA from "react-ga4";
-import ReactPixel from "react-facebook-pixel";
 
 import "./index.css";
 import App from "./App.jsx";
@@ -11,14 +10,33 @@ import { ToastProvider } from "./context/ToastContext";
 // GA4 init
 ReactGA.initialize("G-DPNZJM55JZ");
 
-// Meta Pixel init
-const pixelOptions = {
-  autoConfig: true,
-  debug: false,
-};
+// Meta Pixel
+const PIXEL_ID = "3263994340476127";
 
-ReactPixel.init("3263994340476127", pixelOptions);
-ReactPixel.pageView();
+(function () {
+  if (typeof window === "undefined") return;
+  if (window.fbq) return;
+
+  window.fbq = function () {
+    window.fbq.callMethod
+      ? window.fbq.callMethod.apply(window.fbq, arguments)
+      : window.fbq.queue.push(arguments);
+  };
+
+  window._fbq = window.fbq;
+  window.fbq.push = window.fbq;
+  window.fbq.loaded = true;
+  window.fbq.version = "2.0";
+  window.fbq.queue = [];
+
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = "https://connect.facebook.net/en_US/fbevents.js";
+  document.head.appendChild(script);
+
+  window.fbq("init", PIXEL_ID);
+  window.fbq("track", "PageView");
+})();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
